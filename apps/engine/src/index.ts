bbddd7c782dd6e04b96d3327415f2a17bfcb7f39
rishipatel9 <rishipatel0826@gmail.com>
2024-09-10 16:46:44 +0530

    added code execution

--- apps/engine/src/index.ts
+++ apps/engine/src/index.ts
@@ -1,1 +1,27 @@
-console.log("hello");
+const express = require('express');
+const bodyParser = require('body-parser');
+const { exec } = require('child_process');
+const cors=require('cors')
+
+const app = express();
+app.use(cors({
+    origin:"http://localhost:3000",
+    credentials:true
+}))
+app.use(bodyParser.json());
+
+app.post('/run-code', (req:any, res:any) => {
+  const code = req.body.code;
+  console.log(code);
+  exec(`node -e "${code}"`, (error:any, stdout:any, stderr:any) => {
+    if (error) {
+      res.status(400).json({ error: stderr });
+    } else {
+      res.status(200).json({ output: stdout });
+    }
+  });
+});
+
+app.listen(3001, () => {
+  console.log('Code execution server running on port 3001');
+});
