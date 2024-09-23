b1972f17d1ab1453f0069b6eadd5769fff6aad30
rishipatel9 <rishipatel0826@gmail.com>
2024-09-23 21:59:34 +0530

    updated Dockerfile

--- apps/engine/src/index.ts
+++ apps/engine/src/index.ts
@@ -11,12 +11,35 @@ app.use(cors({
 }));
 app.use(bodyParser.json());
 
-app.post('/run-code', (req:any, res:any) => {
+app.post('/run-code', (req: any, res: any) => {
     let code = req.body.code;
+    const lang = req.body.selectedLanguage;
+    console.log('Selected Language: ', lang);
     console.log('Code received: ', code);
     code = code.replace(/"/g, '\\"');
 
-    exec(`./run_in_docker.sh "${code}"`, { cwd: path.resolve(__dirname) }, (error: any, stdout: any, stderr: any) => {
+    let command = "";
+
+    switch (lang) {
+        case "cpp":
+            command = `echo "${code}" > code.cpp && g++ code.cpp -o code.out && ./code.out`;
+            break;
+        case "py":
+            command = `echo "${code}" > code.py && python3 code.py`;
+            break;
+        case "js":
+            command = `echo "${code}" > code.js && node code.js`;
+            break;
+        case "java":
+            command = `echo "${code}" > Main.java && javac Main.java && java Main`;
+            break;
+        case "ts":
+            command = `echo "${code}" > npx tsc -b && code.ts && node code.js`;
+        default:
+            return res.status(400).json({ error: "Unsupported language" });
+    }
+
+    exec(command, { cwd: path.resolve(__dirname) }, (error: any, stdout: any, stderr: any) => {
         if (error) {
             console.error('Error executing code:', stderr);
             return res.status(400).json({ error: stderr });
@@ -26,6 +49,7 @@ app.post('/run-code', (req:any, res:any) => {
         return res.status(200).json({ output: stdout });
     });
 });
+
 
 app.listen(3001, () => {
     console.log('Code execution server running on port 3001');
